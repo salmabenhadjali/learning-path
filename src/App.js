7d75f6d477594c47f6ee018ebd6f905091b2acc2
sbenhadjali <salma.benhadjali@veolia.com>
2024-09-10 12:32:44 +0200

    feat: learn CSS
    activate only ui container

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header class="container">
+        <h1>CSS Box Model</h1>
       </header>
+      
+      <main class="container">
+        <p>
+          L'élevage de la Fougère Aigle vous accueille sur son site et vous présente ses différents services. 
+          Découvrez le Setter Anglais, chien de race, idéal pour la chasse à la bécasse.
+          L'élevage de Setters Anglais de " La Fougère Aigle " se situe au cœur de la Bretagne, dans une région boisée, riche d'un remarquable biotope à bécasse.
+          Les dressages et entrainements ont lieu sur des territoires très étendus, riches en densité d'oiseaux où les chiens peuvent exprimer tout leur potentiel.
+        </p>
+
+        {/* <form>
+          <label for="name">Name: </label>
+          <input id="name" type="text" placeholder="Your Name"/>
+          <button>Submit</button>
+        </form> */}
+      </main>
     </div>
   );
 }
