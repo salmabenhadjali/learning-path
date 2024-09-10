ad7b1ef5adb6217ec9174f9c051a5dcbac0bd787
sbenhadjali <salma.benhadjali@veolia.com>
2024-09-10 22:02:10 +0200

    feat: create menu + routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <header class="container">
-        <h1>CSS Box Model</h1>
-      </header>
-      
-      <main class="container">
-        <p>
-          L'élevage de la Fougère Aigle vous accueille sur son site et vous présente ses différents services. 
-          Découvrez le Setter Anglais, chien de race, idéal pour la chasse à la bécasse.
-          L'élevage de Setters Anglais de " La Fougère Aigle " se situe au cœur de la Bretagne, dans une région boisée, riche d'un remarquable biotope à bécasse.
-          Les dressages et entrainements ont lieu sur des territoires très étendus, riches en densité d'oiseaux où les chiens peuvent exprimer tout leur potentiel.
-        </p>
-
-        {/* <form>
-          <label for="name">Name: </label>
-          <input id="name" type="text" placeholder="Your Name"/>
-          <button>Submit</button>
-        </form> */}
-      </main>
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ ui/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+    return (
+      <main>
+        <p>
+          Learn more about <span class="opposite">Our Genitors</span>
+        </p>
+      </main>
+    );
+}
+
+export default Home;
