672ac60a12715c4f3485f7232b0ac50fc4657e47
sbenhadjali <salma.benhadjali@veolia.com>
2024-09-13 15:39:18 +0200

    feat: [Chapter 11] Floats

--- ui/src/pages/About.js
+++ ui/src/pages/About.js
@@ -1,8 +1,31 @@
 import React from "react";
+import './About.css';
 
 const About = () => {
     return (
-        <h1>Welcome to about page</h1>
+        <div>
+            <section>
+                <div class="block left">Float Left</div>
+                <p>Lorem paragraphe</p>
+                <p>Lorem paragraphe</p>
+            </section>
+        
+            <div class="clear"></div>
+            <p>Lorem paragraphe</p>
+            <div class="block right">Float Right</div>
+            <p>Lorem paragraphe</p>
+            <p>Lorem paragraphe</p>
+            <p>Lorem paragraphe Lorem paragraphe Lorem paragraphe Lorem paragraphe</p>
+            <p>Lorem paragraphe Lorem paragraphe Lorem paragraphe Lorem paragraphe Lorem paragraphe</p>
+            <p>Lorem paragraphe Lorem paragraphe Lorem paragraphe Lorem paragrapheLorem paragraphe</p>
+            <p>Lorem paragraphe</p>
+            <p>Lorem paragraphe</p>
+            <div class="clear"></div>
+            <p>Lorem paragraphe</p>
+            <p>Lorem paragraphe</p>
+            <p>Lorem paragraphe</p>
+            <p>Lorem paragraphe</p>
+        </div>
     );
 }
 
