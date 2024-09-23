293fc270c91b5f591e7e8687130355954d10416a
sbenhadjali <salma.benhadjali@veolia.com>
2024-09-23 22:39:40 +0200

    feat: [Chapter 16] Images

---
+++ src/pages/Images/Images.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Images.css';
+
+const Images = () => {
+    return (
+        <div class="images">
+            {/* <section className="example">
+                <p>
+                    <img src={`${process.env.PUBLIC_URL}/assets/1-Tell.png`} alt="Tell" width="122" height="200"/>
+                </p>
+            </section> */}
+            {/* <section className="hero">
+                <figure className="profil-pic-figure">
+                    <img src={`${process.env.PUBLIC_URL}/assets/avatar.png`} alt="Profile Picture" width="200" height="200"/>
+                    <figcaption class="offscreen">Jane Doe</figcaption>
+                </figure>
+                <h1 class="h1">
+                    <span className="nowrap">Hello 👋</span> 
+                    <span className="nowrap">I'm Jane</span>
+                </h1>
+            </section> */}
+            <section class="clip">Jane</section>
+        </div>
+    );
+}
+
+export default Images;
