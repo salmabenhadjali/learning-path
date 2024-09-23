899d23a65d06603e33f160e898ff9b895a51b0aa
sbenhadjali <salma.benhadjali@veolia.com>
2024-09-23 22:39:40 +0200

    feat: [Chapter 16] Images

--- ui/src/pages/Images/Images.js
+++ ui/src/pages/Images/Images.js
@@ -3,8 +3,23 @@ import './Images.css';
 
 const Images = () => {
     return (
-        <div>
-
+        <div class="images">
+            {/* <section className="example">
+                <p>
+                    <img src={`${process.env.PUBLIC_URL}/assets/1-Tell.png`} alt="Tell" width="122" height="200"/>
+                </p>
+            </section> */}
+            {/* <section className="hero">
+                <figure className="profil-pic-figure">
+                    <img src={`${process.env.PUBLIC_URL}/assets/avatar.png`} alt="Profile Picture" width="200" height="200"/>
+                    <figcaption class="offscreen">Jane Doe</figcaption>
+                </figure>
+                <h1 class="h1">
+                    <span className="nowrap">Hello 👋</span> 
+                    <span className="nowrap">I'm Jane</span>
+                </h1>
+            </section> */}
+            <section class="clip">Jane</section>
         </div>
     );
 }
