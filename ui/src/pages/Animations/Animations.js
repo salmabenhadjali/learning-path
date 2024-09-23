50e33c85bbda83fabea4768456a74b8da0ee762f
sbenhadjali <salma.benhadjali@veolia.com>
2024-09-23 12:26:57 +0200

    feat: refacto navigation

---
+++ ui/src/pages/Animations/Animations.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Animations.css';
+
+const Animations = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Animations;
---
+++ ui/src/pages/MediaQueries/MediaQueries.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './MediaQueries.css';
+
+const MediaQueries = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default MediaQueries;
