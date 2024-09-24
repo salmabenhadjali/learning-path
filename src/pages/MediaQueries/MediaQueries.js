b40d0d183336d8d5ee8c2691070a8d43b21f1f41
sbenhadjali <salma.benhadjali@veolia.com>
2024-09-24 11:59:12 +0200

    feat: [Chapter 17] Media Queries

--- src/pages/MediaQueries/MediaQueries.js
+++ src/pages/MediaQueries/MediaQueries.js
@@ -3,8 +3,19 @@ import './MediaQueries.css';
 
 const MediaQueries = () => {
     return (
-        <div>
+        <div class="media-queries">
+            <header>
+                <h1>Header</h1>
+                <nav><h2>Nav</h2></nav>
+            </header>
 
+            <main>
+                <h2>Main</h2>
+            </main>
+
+             <footer>
+                <h2>Footer</h2>
+            </footer>
         </div>
     );
 }
