0d5d6bdc800eaf9aa5be2be1f3b432665a0a958f
sbenhadjali <salma.benhadjali@veolia.com>
2024-10-04 16:13:41 +0200

    feat: [Chapter 20] Variables

--- src/pages/Variables/Variables.js
+++ src/pages/Variables/Variables.js
@@ -3,8 +3,30 @@ import './Variables.css';
 
 const Variables = () => {
     return (
-        <div>
+        <div class="variables">
+            <header>
+                <h1>Header</h1>
+                <nav><h2>Nav</h2></nav>
+            </header>
 
+            <main>
+                <div className="square"><p>Hey</p></div>
+                <div className="square">💻</div>
+                <div className="square">💻</div>
+                <div className="square">💻</div>
+                <div className="square">💻</div>
+                <div className="square">💻</div>
+                <div className="square">💻</div>
+                <div className="square square--highlight">💻</div>
+                <div className="square">💻</div>
+                <div className="square">💻</div>
+                <div className="square">💻</div>
+                <div className="square">💻</div>
+            </main>
+
+             <footer>
+                <h2>Footer</h2>
+            </footer>
         </div>
     );
 }
