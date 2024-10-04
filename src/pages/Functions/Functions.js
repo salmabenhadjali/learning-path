317ec75a01c6ddb0ba4547349becc33e5307759b
sbenhadjali <salma.benhadjali@veolia.com>
2024-10-04 17:11:42 +0200

    feat: [Chapter 21] Functions
    Part 1: General functions

--- src/pages/Functions/Functions.js
+++ src/pages/Functions/Functions.js
@@ -3,8 +3,37 @@ import './Functions.css';
 
 const Functions = () => {
     return (
-        <div>
+        <div class="functions">
+            <header>
+                <h1>Header</h1>
+                <nav><h2>Nav</h2></nav>
+            </header>
 
+            <main>
+                <section>
+                    <h2>My heading</h2>
+                    <p className="content">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias perferendis 
+                        veritatis cum tenetur illum, quod voluptate dolor vero? Porro explicabo soluta 
+                        pariatur veritatis ullam distinctio adipisci, qui minus voluptate inventore!
+                    </p>
+                    <br></br>
+                    <a href="http://www.google.com">Google</a> 
+                </section>
+                <aside>
+                    <h2>Aside</h2>
+                    <p className="content">
+                        <span className="tooltip" data-tooltip="This is Latin">Lorem</span> ipsum 
+                        dolor, sit amet consectetur adipisicing elit. Alias perferendis veritatis 
+                        cum tenetur illum, quod voluptate dolor vero? Porro explicabo soluta pariatur 
+                        veritatis ullam distinctio adipisci, qui minus voluptate inventore!
+                    </p>
+                </aside>
+            </main>
+
+             <footer>
+                <h2>Footer</h2>
+            </footer>
         </div>
     );
 }
