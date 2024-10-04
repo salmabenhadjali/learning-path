07ec331957211184b025e34e3b59bc2a254fdabb
sbenhadjali <salma.benhadjali@veolia.com>
2024-10-04 20:04:10 +0200

    feat: [Chapter 22] Animations

--- ui/src/pages/Animations/Animations.js
+++ ui/src/pages/Animations/Animations.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './Animations.css';
 
 const Animations = () => {
     return (
-        <div>
+        // <div class="animations">
+        //     <main>
+        //         <div className="square">🚀</div>
+        //         <div className="square">🔔</div>
+        //         <div className="square animate">🎷</div>
+        //     </main>
+        // </div>
 
+        <div class="animations">
+            <header>
+                <section className="header-title-line">
+                    <h1>Acme Co.</h1>
+                    <button className="menu-button">
+                        <div className="menu-icon"></div>
+                    </button>
+                </section>
+                <nav>
+                    <ul>
+                        <li><Link to="/products">Products</Link></li> 
+                        <li><Link to="#">Forum</Link></li> 
+                        <li><Link to="#">About</Link></li> 
+                        <li><Link to="#">Contact</Link></li> 
+                        {/* <li><a href="#"></a>Forum</li> 
+                        <li><a href="#"></a>About</li> 
+                        <li><a href="#"></a>Contact</li>  */}
+                    </ul>
+                </nav>
+            </header>
         </div>
     );
 }
---
+++ ui/src/pages/Animations/Products.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Products = () => {
+    return (
+        <div class="products">
+            Products
+        </div>
+    );
+}
+
+export default Products;
