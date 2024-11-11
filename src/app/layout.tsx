bec4f5e3f7109a5596184a9c1ccb4849904fc5a3
salmabenhadjali <salma.belhadjali@gmail.com>
2024-11-11 13:10:40 +0100

    feat: NextJS Rendering SSR & CSR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import { Inter, Nunito } from "next/font/google"
+import { Nunito } from "next/font/google"
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-const nunito = Inter({subsets: ["latin"]})
+const nunito = Nunito({
+  subsets: ["latin"], 
+  weight: ["400", "900"]
+})
 
 export const metadata: Metadata = {
   title: "Nextfolio",
