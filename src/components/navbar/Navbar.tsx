c14291074fd2b00cac94b61a07a9e3f2dd9167f7
salmabenhadjali <salma.belhadjali@gmail.com>
2024-11-11 13:10:40 +0100

    feat: NextJS Rendering SSR & CSR

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,12 @@
+"use client"
 import Link from "next/link"
 import { menuItems } from "@/data/menuItems"
 import "./Navbar.css"
 
 const Navbar = () => {
+    const logout = () => {
+        console.log("logged out !")
+    }
     return (
         <div>
             <Link href="/">NextFolio</Link>
@@ -10,6 +14,7 @@ const Navbar = () => {
                 {menuItems.map(item => (
                     <Link key={item.id} href={item.url}>{item.title}</Link>
                 ))}
+                <button onClick={logout}>Logout</button>
             </div>
         </div>
     )
