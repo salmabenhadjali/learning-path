629e210189a70e1fc9c91bc0a3f77177182e985f
salmabenhadjali <salma.belhadjali@gmail.com>
2024-12-21 07:49:27 +0100

    feat: Next.js Data Fetching on The Server Side

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { FC } from "react";
+import { notFound } from "next/navigation";
 
-export default function BlogPost() {
+async function getData(id: string) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store", //default caching in Version 15+
+    // cache: "force-cache", //default caching in Version 13
+    // next: { revalidate: 10 }, //Change lifetime is 10s
+  });
+  if (!res.ok) {
+    return notFound();
+  }
+  return res.json();
+}
+
+const BlogPost: FC<{ params: { id: string } }> = async ({ params }) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -9,12 +24,7 @@ export default function BlogPost() {
           <h1 className={styles.title}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit
           </h1>
-          <p className={styles.desription}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam autem
-            laboriosam consectetur ipsam aliquid iste neque, perspiciatis totam
-            fugit architecto. Nesciunt architecto atque totam ipsam quidem
-            magnam. Ullam, saepe aspernatur!
-          </p>
+          <p className={styles.desription}>{data.body}</p>
           <div className={styles.author}>
             <Image
               src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
@@ -45,4 +55,6 @@ export default function BlogPost() {
       </div>
     </div>
   );
-}
+};
+
+export default BlogPost;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,51 +4,39 @@ import Image from "next/image";
 import { FC } from "react";
 import Link from "next/link";
 
-const Blog: FC<{ params: { category: string } }> = ({ params }) => {
-  console.log(params);
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store", //default caching in Version 15+
+    // cache: "force-cache", //default caching in Version 13
+    // next: { revalidate: 10 }, //Change lifetime is 10s
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
   return (
     <div className={styles.container}>
-      <h1 className={styles.categoryTitle}>{params.category}</h1>
       <div className={styles.items}>
-        <Link className={styles.item} href="blog/id">
-          <div className={styles.content}>
-            <h1 className={styles.contentTitle}>Creative Portfolio</h1>
-            <p className={styles.contentDescription}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Dignissimos blanditiis tenetur molestias excepturi dolorem porro
-              voluptatibus nihil consequuntur numquam sed atque perspiciatis
-              nisi dolore aut temporibus nesciunt quaerat, ad quos.
-            </p>
-          </div>
-          <div className={styles.imgContainer}>
-            <Image
-              className={styles.img}
-              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-              fill={true}
-              alt=""
-            />
-          </div>
-        </Link>
-
-        <Link className={styles.item} href="blog/id">
-          <div className={styles.content}>
-            <h1 className={styles.contentTitle}>Creative Portfolio</h1>
-            <p className={styles.contentDescription}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Dignissimos blanditiis tenetur molestias excepturi dolorem porro
-              voluptatibus nihil consequuntur numquam sed atque perspiciatis
-              nisi dolore aut temporibus nesciunt quaerat, ad quos.
-            </p>
-          </div>
-          <div className={styles.imgContainer}>
-            <Image
-              className={styles.img}
-              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-              fill={true}
-              alt=""
-            />
-          </div>
-        </Link>
+        {data.map((item) => (
+          <Link key={item.id} className={styles.item} href={`blog/${item.id}`}>
+            <div className={styles.content}>
+              <h1 className={styles.contentTitle}>{item.title}</h1>
+              <p className={styles.contentDescription}>{item.body}</p>
+            </div>
+            <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+                fill={true}
+                alt=""
+              />
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
