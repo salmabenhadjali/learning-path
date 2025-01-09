bc0e0baf4131e748dc20fa73dfeb7218e6e41a4a
Salma BEN HADJ ALI <salma.benhadjali.io@gmail.com>
2025-01-09 16:40:24 +0100

    feat: Wildcard Routes

--- app/src/Controller/VinylController.php
+++ app/src/Controller/VinylController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
@@ -13,5 +14,17 @@ class VinylController
     function homepage(): Response
     {
         return new Response('Hello worl from Response !');
+    }
+
+    #[Route('/browse/{slug}')]
+    function browse(?string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre : ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = "All genres";
+        }
+
+        return new Response($title);
     }
 }
