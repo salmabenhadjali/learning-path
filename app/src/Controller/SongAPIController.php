0a20ebd8ed68d0820950b7e7f2b7fa20513d98e6
Salma BEN HADJ ALI <salma.benhadjali.io@gmail.com>
2025-01-13 15:40:01 +0100

    feat: Service Objects

--- app/src/Controller/SongAPIController.php
+++ app/src/Controller/SongAPIController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SongAPIController extends AbstractController
 {
     #[Route('/api/song/{id<\d+>}', methods: ['GET'])]
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         //TODO query the database
         $song = [
@@ -20,6 +21,10 @@ class SongAPIController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('request from API for song {sonng}', [
+            'song' => $song
+        ]);
 
         // return new JsonResponse($song);
         return $this->json($song);
