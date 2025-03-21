a74a79ba4b1cff86f8b910764c7e5b5f4b5b1203
Salma BEN HADJ ALI <salma.benhadjali.io@gmail.com>
2025-03-21 11:48:34 +0100

    feat: some VueJS directives
    v-bind, v-html, v-once, v-on

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <template>
-    <div>
-      <h1>Hello, Vue 2 with Vite! 🚀</h1>
-    </div>
-  </template>
-  
-  <script>
+  <div>
+    <h1 v-once>{{ title }}</h1>
+    <p>{{ sayHello() }} - <a v-bind:href="link">Google</a></p>
+    <p v-html="finalLink"></p>
+
+    <button v-on:click="increase(2, $event)">Click me</button>
+    <p>{{ counter }}</p>
+
+    <p v-on:mousemove="updateCoordinates">
+      Coordinates : {{ x }} / {{ y }} - <span v-on:mousemove.stop="">DEAD SPOT</span>
+    </p>
+
+    <input type="text" v-on:keyup.enter.space="alertMe">
+  </div>
+</template>
+
+<script>
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        title : 'Hello, Vue 2 with Vite! 🚀',
+        link: 'http://www.google.com',
+        finalLink: '<a href="http://www.google.com">Google</a>',
+        counter: 0,
+        x: 0,
+        y: 0,
+      }
+    },
+    methods: {
+      sayHello() {
+        this.title = 'Hello!'
+        return this.title
+      },
+
+      increase(step, event) {
+        return this.counter += step
+      },
+
+      updateCoordinates(event) {
+        this.x = event.clientX
+        this.y = event.clientY
+      },
+
+      alertMe() {
+        alert('Alert!')
+      }
+    }
   }
-  </script>
-  
-  <style>
+</script>
+
+<style>
   h1 {
     color: #42b883;
   }
-  </style>
+</style>
   
