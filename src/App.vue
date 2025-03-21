b4610a2b0599493d9661604f7a887efafe4fe297
Salma BEN HADJ ALI <salma.benhadjali.io@gmail.com>
2025-03-21 14:49:16 +0100

    style: shortchuts for v-on and v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <h1 v-once>{{ title }}</h1>
-    <p>{{ sayHello() }} - <a v-bind:href="link">Google</a></p>
+    <p>{{ sayHello() }} - <a :href="link">Google</a></p>
     <p v-html="finalLink"></p>
 
-    <button v-on:click="increase(2, $event)">Increase counter</button>
-    <button v-on:click="counter--">Decrease counter</button>
-    <button v-on:click="secondCounter++">Click Second Counter</button>
+    <button @click="increase(2, $event)">Increase counter</button>
+    <button @click="counter--">Decrease counter</button>
+    <button @click="secondCounter++">Click Second Counter</button>
     <p>{{ counter }} | {{ secondCounter }}</p>
     <p>Result: {{ result() }} | {{ output }}</p>
 
-    <p v-on:mousemove="updateCoordinates">
-      Coordinates : {{ x }} / {{ y }} - <span v-on:mousemove.stop="">DEAD SPOT</span>
+    <p @mousemove="updateCoordinates">
+      Coordinates : {{ x }} / {{ y }} - <span @mousemove.stop="">DEAD SPOT</span>
     </p>
 
-    <input type="text" v-on:keyup.enter.space="alertMe">
+    <input type="text" @keyup.enter.space="alertMe">
     <br><br><br><br>
 
     <input type="text" v-model="name">
